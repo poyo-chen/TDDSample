c5760632332c5d7ebbd2b455728d14c3fd75cb2f
martinezchen <martinezchen01@gmail.com>
2022-05-09 23:55:42 +0800

    TDD Sample BowlingGameTest and BowlingGame 調整testOneSpare

--- src/main/java/source/BowlingGame.java
+++ src/main/java/source/BowlingGame.java
@@ -1,18 +1,23 @@
 package source;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class BowlingGame {
     private int[] rows = new int[20];
     private int count = 0;
     private int score;
 
     public void roll(int i) {
-        rows[count++] = i;
-        if (rows[1] + rows[0] == 10) {
-            score = rows[2] * 2 + 10;
-        }
+        rows[count] = i;
+        count++;
     }
 
     public int score() {
+        for (int i = 0; i < rows.length; i++){
+            score+=rows[i];
+        }
+        Arrays.stream(rows).forEach(System.out::println);
         return score;
     }
 }
